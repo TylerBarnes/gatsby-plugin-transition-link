c3fce36f8fefdd7485040caf0cc436dffc715090
Tyler Barnes <tyler@bare.ca>
2018-10-22 22:20:56 -0700

    remove react-context-consumer-hoc dependency

--- example/plugins/gatsby-plugin-transition-link/components/TransitionHandler.js
+++ example/plugins/gatsby-plugin-transition-link/components/TransitionHandler.js
@@ -1,44 +1,40 @@
 import React from 'react'
 import { Transition, TransitionGroup } from 'react-transition-group'
 import { Consumer } from '../store/createContext'
-import { withContext } from 'react-context-consumer-hoc'
 import AppProvider from '../store/provider'
-
-const TransitionWithContext = withContext(
-  [Consumer],
-  context => ({
-    timeout: {
-      enter: context.delayNext,
-      exit: context.exitTimeout,
-    },
-  }) // mapContextToProps
-)(Transition)
 
 const TransitionHandler = props => {
   return (
     <AppProvider>
-      <TransitionGroup>
-        <TransitionWithContext key={props.location.pathname}>
-          {state => {
-            // use transition groups entering state to hide the incoming component
-            // supposedly this should instead be done with a setTimeout and the in prop... https://github.com/reactjs/react-transition-group/issues/284
-            // This works for now.
-            const visibility = state === 'entering' ? 'hidden' : 'visible'
+      <Consumer>
+        {({ delayNext, exitTimeout }) => (
+          <TransitionGroup>
+            <Transition
+              timeout={{ enter: delayNext, exit: exitTimeout }}
+              key={props.location.pathname}
+            >
+              {state => {
+                // use transition groups entering state to hide the incoming component
+                // supposedly this should instead be done with a setTimeout and the in prop... https://github.com/reactjs/react-transition-group/issues/284
+                // This works for now.
+                const visibility = state === 'entering' ? 'hidden' : 'visible'
 
-            return (
-              <div
-                style={{
-                  position: 'absolute',
-                  width: '100%',
-                  visibility: visibility,
-                }}
-              >
-                {props.children}
-              </div>
-            )
-          }}
-        </TransitionWithContext>
-      </TransitionGroup>
+                return (
+                  <div
+                    style={{
+                      position: 'absolute',
+                      width: '100%',
+                      visibility: visibility,
+                    }}
+                  >
+                    {props.children}
+                  </div>
+                )
+              }}
+            </Transition>
+          </TransitionGroup>
+        )}
+      </Consumer>
     </AppProvider>
   )
 }
