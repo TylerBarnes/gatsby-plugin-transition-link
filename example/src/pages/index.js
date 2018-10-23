bce2cd7e01fcb849db8128dc008c20165af87835
Tyler Barnes <tyler@bare.ca>
2018-10-23 00:59:07 -0700

    trying to get this thing to build...

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby'
+import { TimelineMax } from 'gsap/all'
 import GSAP from 'react-gsap-enhancer'
-import { TweenMax, TimelineMax } from 'gsap/TweenMax'
 
 import TransitionLink from '../../plugins/gatsby-plugin-transition-link'
 import Layout from '../components/layout'
 
 const moveAnimation = ({ target, options }) => {
-  const totalTime = options.timeout / 1000;
-  
+  const totalTime = options.timeout / 1000
+
   return new TimelineMax()
-  .to(target.find({type: 'transitionCover'}), totalTime / 2, { y: "0%" })
-  .set(target.find({name:"layoutContents"}), {opacity: 0})
-    .to(target.find({type: 'transitionCover'}), totalTime / 2, {y: "-100%"})
+    .to(target.find({ name: 'transitionCover' }), totalTime / 2, { y: '0%' })
+    .set(target.find({ name: 'layoutContents' }), { opacity: 0 })
+    .to(target.find({ name: 'transitionCover' }), totalTime / 2, { y: '-100%' })
 }
-class index extends Component {
+
+class Index extends Component {
   constructor(props) {
     super(props)
 
@@ -36,25 +37,25 @@ class index extends Component {
       <Layout>
         <section name="layoutContents">
           <h1>Hi people</h1>
-        <p>Check out these sick transitions.</p>
+          <p>Check out these sick transitions.</p>
 
-        <Link to="/page-2">Go to page 2 normally</Link>
-        <br />
-        <TransitionLink
-          to="/page-2"
-          exitAnimationTimeout={1000}
-          hideNextFor={600}
-          triggerFn={this.startAnimation}
-          nextState={{
-            animation: 'fromHome',
-          }}
-        >
-          Go to page 2 that way{' '}
-          <span aria-label="pointing down" role="img">
-            👇
-          </span>
-        </TransitionLink>
-        {/* <br />
+          <Link to="/page-2">Go to page 2 normally</Link>
+          <br />
+          <TransitionLink
+            to="/page-2"
+            exitAnimationTimeout={1000}
+            hideNextFor={600}
+            triggerFn={this.startAnimation}
+            nextState={{
+              animation: 'fromHome',
+            }}
+          >
+            Go to page 2 that way{' '}
+            <span aria-label="pointing down" role="img">
+              👇
+            </span>
+          </TransitionLink>
+          {/* <br />
         <TransitionLink
           to="/page-2"
           exitAnimationTimeout={1200}
@@ -88,10 +89,21 @@ class index extends Component {
           y transition
         </TransitionLink> */}
         </section>
-        <div type="transitionCover" style={{ position: "fixed", background: "rebeccapurple", top: 0, left: 0, width: "100vw", height: "100vh", transform: "translateY(100%)"}} />
+        <div
+          name="transitionCover"
+          style={{
+            position: 'fixed',
+            background: 'rebeccapurple',
+            top: 0,
+            left: 0,
+            width: '100vw',
+            height: '100vh',
+            transform: 'translateY(100%)',
+          }}
+        />
       </Layout>
     )
   }
 }
 
-export default GSAP()(index)
+export default GSAP()(Index)
