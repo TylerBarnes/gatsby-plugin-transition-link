9253480f2c1d3062c5b38f594139a7c208159d94
Tyler Barnes <tyler@bare.ca>
2018-10-24 01:24:32 -0700

    ok, actually fix the build step

--- example/src/components/DisplayState.js
+++ example/src/components/DisplayState.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Location } from '@reach/router'
 
 function print_r(o) {
-  if (typeof window !== undefined) {
-    return JSON.stringify(o, null, '\t')
-      .replace(/\n/g, '<br>')
-      .replace(/\t/g, '&nbsp;&nbsp;&nbsp;')
-  } else {
-    return null
-  }
+  if (typeof window === `undefined`) return
+
+  return JSON.stringify(o, null, '\t')
+    .replace(/\n/g, '<br>')
+    .replace(/\t/g, '&nbsp;&nbsp;&nbsp;')
 }
 
 function DisplayState() {
