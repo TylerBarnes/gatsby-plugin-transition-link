8f36b68798192495f18f18c0fc3bd7ec1f12a0e5
Tyler Barnes <tyler@bare.ca>
2018-10-24 00:05:16 -0700

    remove comment

--- example/plugins/gatsby-plugin-transition-link/store/provider.js
+++ example/plugins/gatsby-plugin-transition-link/store/provider.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from './createContext'
 
-// The provider, which holds the page-wide store and its actions.
-// Feel free to abstract actions and state away from this file.
 class AppProvider extends Component {
   state = {
     exitTimeout: 0,
