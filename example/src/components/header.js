5b24d278a6ea87154e00d48c77dedcde3c13d802
Tyler Barnes <tyler@bare.ca>
2018-10-24 21:31:29 -0700

    add link to github

--- example/src/components/header.js
+++ example/src/components/header.js
@@ -1,36 +1,44 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import GithubLink from './GithubLink'
+
 const Header = ({ siteTitle, locationState }) => {
-  const headerColor = locationState && locationState.headerBgColor ? locationState.headerBgColor : "rebeccapurple"
+  const headerColor =
+    locationState && locationState.headerBgColor
+      ? locationState.headerBgColor
+      : 'rebeccapurple'
   return (
-  <div
-    style={{
-      background: headerColor,
-      marginBottom: '1.45rem',
-    }}
-  >
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        background: headerColor,
+        marginBottom: '1.45rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '1.45rem 1.0875rem',
+          paddingRight: "120px"
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+
+      <GithubLink />
     </div>
-  </div>
-)
+  )
 }
 
 export default Header
