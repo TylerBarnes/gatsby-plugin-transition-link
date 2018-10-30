c709ce72c2cc5276e75de9c7f05c178b253792e1
Tyler Barnes <tyler@bare.ca>
2018-10-29 22:42:57 -0700

    Add TransitionPortal

---
+++ src/components/TransitionPortal.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const portalRoot = document.body;
+
+const PortalContainer = props => {
+  const zIndex = (function(level) {
+    switch (level) {
+      case "bottom":
+        return 1000;
+      case "top":
+        return 1200;
+      default:
+        return 1100;
+    }
+  })(props.level);
+
+  return (
+    <div
+      className="gatsby-plugin-transition-link-portal"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        pointerEvents: "none",
+        zIndex: zIndex
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default class TransitionPortal extends Component {
+  constructor() {
+    super();
+    this.el = document.createElement("section");
+  }
+
+  componentDidMount = () => {
+    portalRoot.appendChild(this.el);
+  };
+
+  componentWillUnmount = () => {
+    portalRoot.removeChild(this.el);
+  };
+
+  render() {
+    return ReactDOM.createPortal(
+      <>
+        <PortalContainer styles={this.props.css} level={this.props.level}>
+          {this.props.children}
+        </PortalContainer>
+      </>,
+      this.el
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { TransitionLink } from "./components/TransitionLink";
 import TransitionHandler from "./components/TransitionHandler";
 import { PublicConsumer as TransitionConsumer } from "./context/createTransitionContext";
+import TransitionPortal from "./components/TransitionPortal";
 
-export { TransitionHandler, TransitionConsumer };
+export { TransitionHandler, TransitionConsumer, TransitionPortal };
 export default TransitionLink;
