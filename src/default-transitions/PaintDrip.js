1cd1b2b9b4d65d2ed8ef8b7e2a61fa614215f941
Tyler Barnes <tyler@bare.ca>
2018-11-01 21:50:55 -0700

    attempt at a circle click covering the screen

--- src/default-transitions/PaintDrip.js
+++ src/default-transitions/PaintDrip.js
@@ -1,8 +1,10 @@
+// use this link to get this working properly
+// https://codepen.io/osublake/pen/eNrQqV?editors=0010
 import React, { Component } from "react";
 import TransitionLink, {
   TransitionPortal
 } from "gatsby-plugin-transition-link";
-import { TweenLite } from "gsap";
+import { TimelineMax } from "gsap";
 
 export default class PaintDrip extends Component {
   constructor(props) {
@@ -12,17 +14,32 @@ export default class PaintDrip extends Component {
     this.expand = this.expand.bind(this);
   }
 
-  expand = () =>
-    TweenLite.to(this.drip, 1, { width: "110vw", height: "110vh" });
+  expand = ({ e }) => {
+    if (typeof window === `undefined`) return false;
+
+    const { innerHeight: h, innerWidth: w } = window;
+    const diagonal = Math.sqrt(Math.pow(h, 2) + Math.pow(w, 2));
+    const { pageX, pageY } = e;
+
+    return new TimelineMax()
+      .set(this.drip, { left: pageX, top: pageY })
+      .to(this.drip, 0.2, {
+        scale: diagonal * 2
+      });
+  };
 
   render() {
     const { props } = this;
+
     return (
       <>
         <TransitionLink
           to={props.to}
           entry={{ delay: 1000 }}
-          exit={{ length: 1000, trigger: () => this.expand() }}
+          exit={{
+            length: 1000,
+            trigger: (exit, node, e) => this.expand({ e })
+          }}
         >
           {props.children}
         </TransitionLink>
@@ -35,8 +52,8 @@ export default class PaintDrip extends Component {
               left: "50%",
               top: "50%",
               transform: "translate(-50%, -50%)",
-              width: "10px",
-              height: "10px",
+              width: "1px",
+              height: "1px",
               background: "black",
               borderRadius: "50%"
             }}
