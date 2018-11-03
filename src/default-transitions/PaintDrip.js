12a9e725da1ac1512a5bf2ea3161536dd18409e4
Tyler Barnes <tyler@bare.ca>
2018-11-02 22:13:40 -0700

    add basic exit transition for paintdrip

--- src/default-transitions/PaintDrip.js
+++ src/default-transitions/PaintDrip.js
@@ -2,7 +2,7 @@
 // https://codepen.io/osublake/pen/eNrQqV?editors=0010
 import React, { Component } from "react";
 import TransitionLink from "gatsby-plugin-transition-link";
-import { TimelineMax, Power0 } from "gsap";
+import { TimelineMax, Power1 } from "gsap";
 import convert from 'color-convert';
 
 export default class PaintDrip extends Component {
@@ -12,14 +12,14 @@ export default class PaintDrip extends Component {
     this.createRipple = this.createRipple.bind(this);
   }
 
- createRipple = ({length}, event, hex) => {
+  createRipple = ({length}, event, hex, color) => {
     const body = document.body;
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
     const piTwo = Math.PI * 2;
-    const rgb = hex ? convert.hex.rgb(hex).join(',') : "0,0,255";
 
-    console.log(rgb)
+    let rgb = hex ? convert.hex.rgb(hex).join(',') : "0,0,255";
+    rgb = color ? convert.keyword.rgb(color) : rgb;
 
     canvas.style.zIndex = 10000;
     canvas.style.position = "fixed";
@@ -47,12 +47,12 @@ export default class PaintDrip extends Component {
     };
 
     const seconds = length / 1000;
-    const rippleEnterTime = seconds * .8;
-    const rippleFadeTime = seconds * .2;
+    const rippleEnterTime = seconds;
     
-    new TimelineMax({ onUpdate: drawRipple, onComplete: removeCanvas })  
-      .to(ripple, rippleEnterTime, { alpha: 1, radius: radius })
-      .to(ripple, rippleFadeTime, { alpha: 0 }, seconds - rippleFadeTime);
+    new TimelineMax({ onUpdate: drawRipple, onComplete: () => removeCanvas(seconds / 3) })  
+      .to(ripple, seconds / 4, { alpha: 1,  })
+      .to(ripple, seconds - seconds / 3, { radius: radius, ease: Power1.easeIn }, 0)
+      .to(canvas, seconds / 3, {x: "100%", ease: Power1.easeIn}, `+=.2`);
     
     function drawRipple() {  
       ctx.clearRect(0, 0, vw, vh);  
@@ -66,15 +66,38 @@ export default class PaintDrip extends Component {
 
     window.addEventListener("resize", onResize);
 
-    function removeCanvas() {
-      body.removeChild(canvas);
+    function removeCanvas(wait = 0) {
+      setTimeout(() => {
+        body.removeChild(canvas);
+      }, wait);
     }
 
     function onResize() {    
       vw = canvas.width  = window.innerWidth;
       vh = canvas.height = window.innerHeight;
     }
-}
+  }
+
+  getDirection = (from) => {
+    switch (from) {
+      case "left":
+        return {xPercent: -5}
+      case "right":
+        return {xPercent: 5}
+      case "top":
+        return {yPercent: -5}
+      case "bottom":
+        return {yPercent: 5}
+      default:
+        return {}
+    }
+  }
+
+  slideIn = ({length}, node, from) => {
+    const seconds = length / 1000;
+
+    new TimelineMax().from(node, seconds, {...this.getDirection(from), ease: Power1.easeOut});
+  }
 
   
   componentWillUnmount() {
@@ -92,11 +115,15 @@ export default class PaintDrip extends Component {
     return <>
         <TransitionLink 
         to={props.to} 
-        entry={{ delay: 400 }} 
         exit={{ 
-          length: 700, 
-          trigger: (exit, node, e) => this.createRipple(exit, e, props.hex) 
+          length: 1000, 
+          trigger: (exit, node, e) => this.createRipple(exit, e, props.hex, props.color) 
           }}
+        entry={{ 
+          delay: 800,
+          length: 600,
+          trigger: (entry, node) => this.slideIn(entry, node, 'left')
+          }} 
         >
           {props.children}
         </TransitionLink>
