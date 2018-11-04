3cb2c5d4662edb4d332da2da0e2438b967940909
Tyler Barnes <tyler@bare.ca>
2018-11-04 13:22:20 -0800

    remove service worker

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -16,7 +16,8 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
+    // 'gatsby-plugin-offline',
+    'gatsby-plugin-remove-serviceworker',
     'gatsby-plugin-transition-link',
   ],
 }
