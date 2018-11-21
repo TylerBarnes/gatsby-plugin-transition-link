52c9f01c0cdb3b4a96791310a3c0bd7385fc9b15
Tyler Barnes <tylerdbarnes@gmail.com>
2018-11-21 11:50:09 -0800

    Make scroll history more reliable

--- src/functions/onEnter.js
+++ src/functions/onEnter.js
@@ -1,6 +1,5 @@
 const onEnter = ({
   node,
-  action,
   inTransition,
   entryTrigger,
   entryProps,
@@ -8,13 +7,12 @@ const onEnter = ({
   pathname,
   e
 }) => {
-  // fix scroll jumping when navigating with browser buttons
-  if (typeof action !== `undefined` && action !== "PUSH") {
+  if (inTransition) {
+    window.scrollTo(0, 0);
+  } else {
     const storageKey = `@@scroll|${pathname}`;
     const savedPosition = sessionStorage.getItem(storageKey);
     window.scrollTo(...JSON.parse(savedPosition));
-  } else {
-    window.scrollTo(0, 0);
   }
 
   if (!inTransition) return;
