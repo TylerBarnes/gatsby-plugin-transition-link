94e2b4ea960bd6a478c635986b66b15f853bc5cd
Tyler <tyler@known.design>
2019-01-15 20:58:11 -0800

    connect duration

--- example/src/components/react-spring-animation.js
+++ example/src/components/react-spring-animation.js
@@ -4,13 +4,18 @@ import TransitionLink, { TransitionState } from 'gatsby-plugin-transition-link'
 
 const MySpring = ({ text }) => (
   <TransitionState>
-    {({ transitionStatus }) => {
+    {({ transitionStatus, exit, entry }) => {
       const mount = ['entering', 'entered'].includes(transitionStatus)
+      const duration = mount ? entry.length : exit.length
+
       return (
         <Spring
           to={{
             transform: `translateY(${mount ? 0 : '100px'})`,
             opacity: mount ? 1 : 0,
+          }}
+          config={{
+            duration: duration,
           }}
         >
           {props => <div style={props}>{text}</div>}
