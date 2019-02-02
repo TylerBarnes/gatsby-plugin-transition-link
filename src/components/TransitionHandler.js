f03f606cfededa4a4930d1ac6824e668ee916784
Tyler <tyler@known.design>
2019-02-02 01:16:30 -0800

    remove reach router location import

--- src/components/TransitionHandler.js
+++ src/components/TransitionHandler.js
@@ -3,7 +3,6 @@ import { Transition, TransitionGroup } from "react-transition-group";
 import { Consumer } from "../context/createTransitionContext";
 import delayTransitionRender from "./delayTransitionRender";
 import { returnTransitionState } from "../utils/returnTransitionState";
-import { Location } from "@reach/router";
 import { getMs } from "../utils/secondsMs";
 import { onEnter } from "../functions/onEnter";
 import { onExit } from "../functions/onExit";
@@ -14,7 +13,10 @@ const DelayedTransition = delayTransitionRender(Transition);
 export default class TransitionHandler extends Component {
   render() {
     const { props } = this;
-    const { children } = props;
+    const {
+      children,
+      location: { action, pathname }
+    } = props;
     return (
       <Consumer>
         {context => {
@@ -32,80 +34,76 @@ export default class TransitionHandler extends Component {
           } = context;
 
           return (
-            <Location>
-              {({ location: { action, pathname } }) => (
-                <Layout {...props}>
-                  <div
-                    className="tl-wrapper-outer"
-                    style={{ maxWidth: "100%", overflowX: "hidden" }}
+            <Layout {...props}>
+              <div
+                className="tl-wrapper-outer"
+                style={{ maxWidth: "100%", overflowX: "hidden" }}
+              >
+                <TransitionGroup component={null}>
+                  <DelayedTransition
+                    key={pathname} // we're using seconds but transitiongroup uses ms
+                    delay={getMs(entryDelay)}
+                    timeout={{
+                      enter: getMs(entryLength),
+                      exit: getMs(exitLength)
+                    }}
+                    onEnter={node =>
+                      !!node &&
+                      onEnter({
+                        node,
+                        action,
+                        pathname,
+                        ...context
+                      })
+                    }
+                    onExit={node =>
+                      !!node &&
+                      onExit({
+                        node,
+                        ...context
+                      })
+                    }
                   >
-                    <TransitionGroup component={null}>
-                      <DelayedTransition
-                        key={pathname} // we're using seconds but transitiongroup uses ms
-                        delay={getMs(entryDelay)}
-                        timeout={{
-                          enter: getMs(entryLength),
-                          exit: getMs(exitLength)
-                        }}
-                        onEnter={node =>
-                          !!node &&
-                          onEnter({
-                            node,
-                            action,
-                            pathname,
-                            ...context
-                          })
+                    {transitionStatus => {
+                      const transitionState = returnTransitionState({
+                        inTransition,
+                        location: props.location,
+                        transitionIdHistory,
+                        transitionStatus,
+                        entry: {
+                          state: entryState,
+                          delay: entryDelay,
+                          length: entryLength
+                        },
+                        exit: {
+                          state: exitState,
+                          delay: exitDelay,
+                          length: exitLength
                         }
-                        onExit={node =>
-                          !!node &&
-                          onExit({
-                            node,
-                            ...context
-                          })
+                      });
+
+                      const childWithTransitionState = React.cloneElement(
+                        children,
+                        {
+                          ...transitionState
                         }
-                      >
-                        {transitionStatus => {
-                          const transitionState = returnTransitionState({
-                            inTransition,
-                            location: props.location,
-                            transitionIdHistory,
-                            transitionStatus,
-                            entry: {
-                              state: entryState,
-                              delay: entryDelay,
-                              length: entryLength
-                            },
-                            exit: {
-                              state: exitState,
-                              delay: exitDelay,
-                              length: exitLength
-                            }
-                          });
+                      );
 
-                          const childWithTransitionState = React.cloneElement(
-                            children,
-                            {
-                              ...transitionState
-                            }
-                          );
-
-                          return (
-                            <PageWrapper
-                              exitProps={exitProps}
-                              entryProps={entryProps}
-                              transitionState={transitionState}
-                              transitionStatus={transitionStatus}
-                            >
-                              {childWithTransitionState}
-                            </PageWrapper>
-                          );
-                        }}
-                      </DelayedTransition>
-                    </TransitionGroup>
-                  </div>
-                </Layout>
-              )}
-            </Location>
+                      return (
+                        <PageWrapper
+                          exitProps={exitProps}
+                          entryProps={entryProps}
+                          transitionState={transitionState}
+                          transitionStatus={transitionStatus}
+                        >
+                          {childWithTransitionState}
+                        </PageWrapper>
+                      );
+                    }}
+                  </DelayedTransition>
+                </TransitionGroup>
+              </div>
+            </Layout>
           );
         }}
       </Consumer>
