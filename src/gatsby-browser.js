f879140ad412af7a12cde59ecc01c66ce89efa80
Tyler Barnes <tyler@known.design>
2019-03-24 22:20:17 -0700

    feat: prevent the back button during transitions
    Previously pressing the back button during transitions could cause the page to look glitchy or elements to dissapear. This prevents that.

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,3 +1,16 @@
+const { navigate } = require("gatsby");
+
 exports.wrapPageElement = require(`./wrap-page`);
 
 exports.shouldUpdateScroll = () => false;
+
+exports.onPreRouteUpdate = ({ location }) => {
+  // prevent the back button during transitions as it breaks pages
+  if (
+    window.__tl_inTransition &&
+    location.pathname !== window.__tl_desiredPathname
+  ) {
+    window.__tl_back_button_pressed = true;
+    navigate(window.__tl_desiredPathname);
+  }
+};
