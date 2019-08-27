55d7fbb9a024d7b461f04256d832a27139168356
Tyler Barnes <tylerdbarnes@gmail.com>
2019-08-26 22:57:14 -0700

    formatting

--- src/wrap-page.js
+++ src/wrap-page.js
@@ -1,8 +1,6 @@
-const React = require("react");
-const TransitionHandler = require("./components/TransitionHandler").default;
+const React = require("react")
+const TransitionHandler = require("./components/TransitionHandler").default
 
 module.exports = ({ element, props }) => {
-  return (
-    <TransitionHandler {...props}>{element}</TransitionHandler>
-  );
-};
+	return <TransitionHandler {...props}>{element}</TransitionHandler>
+}
