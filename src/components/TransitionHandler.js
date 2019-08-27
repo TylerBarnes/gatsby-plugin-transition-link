c41c18a8c2fe34eb59366cd671fe81501f6ebc04
Tyler Barnes <tylerdbarnes@gmail.com>
2019-08-26 22:41:50 -0700

    BREAKING CHANGE: revert to the old way of positioning pages, removing support for persistent footers in gatsby-plugin-layout but improving animations

--- src/components/TransitionHandler.js
+++ src/components/TransitionHandler.js
@@ -1,141 +1,159 @@
-import React, { Component } from "react";
-import { Transition, TransitionGroup } from "react-transition-group";
-import { Location } from "@reach/router";
+import React, { Component } from "react"
+import { Transition, TransitionGroup } from "react-transition-group"
+import { Location } from "@reach/router"
 
-import TransitionRenderer from "./TransitionRenderer";
-import { LayoutComponent as Layout } from "./Layout";
-import delayTransitionRender from "./delayTransitionRender";
-import { Consumer } from "../context/createTransitionContext";
-import { returnTransitionState } from "../utils/returnTransitionState";
-import { onEnter } from "../functions/onEnter";
-import { onExit } from "../functions/onExit";
-import { getMs } from "../utils/secondsMs";
+import TransitionRenderer from "./TransitionRenderer"
+import delayTransitionRender from "./delayTransitionRender"
+import { Consumer } from "../context/createTransitionContext"
+import { returnTransitionState } from "../utils/returnTransitionState"
+import { onEnter } from "../functions/onEnter"
+import { onExit } from "../functions/onExit"
+import { getMs } from "../utils/secondsMs"
 
-import "../style.css";
+import "../style.css"
 
-const DelayedTransition = delayTransitionRender(Transition);
+const DelayedTransition = delayTransitionRender(Transition)
 export default class TransitionHandler extends Component {
-  render() {
-    const { props } = this;
-    const { children } = props;
-    return (
-      <Consumer>
-        {({
-          exitDelay,
-          exitLength,
-          exitState,
-          entryDelay,
-          entryLength,
-          entryState,
-          entryTrigger,
-          entryProps,
-          exitTrigger,
-          exitProps,
-          transitionIdHistory,
-          inTransition,
-          updateContext,
-          triggerResolve,
-          appearAfter,
-          e
-        }) => {
-          return (
-            <Location>
-              {({ location: { action, pathname } }) => (
-                <Layout {...props}>
-                  <div className="tl-edges">
-                    <TransitionGroup component={null}>
-                      <DelayedTransition
-                        key={pathname} // we're using seconds but transitiongroup uses ms
-                        delay={getMs(entryDelay)}
-                        timeout={{
-                          enter: getMs(entryLength),
-                          exit: getMs(exitLength)
-                        }}
-                        onEnter={node =>
-                          !!node &&
-                          !window.__tl_back_button_pressed &&
-                          onEnter({
-                            node,
-                            action,
-                            inTransition,
-                            entryTrigger,
-                            entryProps,
-                            exitProps,
-                            pathname,
-                            updateContext,
-                            triggerResolve,
-                            appearAfter: getMs(appearAfter),
-                            e
-                          })
-                        }
-                        onExit={node =>
-                          !!node &&
-                          !window.__tl_back_button_pressed &&
-                          onExit({
-                            node,
-                            inTransition,
-                            exitTrigger,
-                            entryProps,
-                            exitProps,
-                            triggerResolve,
-                            e
-                          })
-                        }
-                      >
-                        {transitionStatus => {
-                          const mount =
-                            transitionStatus === "entering" ||
-                            transitionStatus === "entered";
+	render() {
+		const { props } = this
+		const { children } = props
+		return (
+			<Consumer>
+				{({
+					exitDelay,
+					exitLength,
+					exitState,
+					entryDelay,
+					entryLength,
+					entryState,
+					entryTrigger,
+					entryProps,
+					exitTrigger,
+					exitProps,
+					transitionIdHistory,
+					inTransition,
+					updateContext,
+					triggerResolve,
+					appearAfter,
+					e,
+				}) => {
+					return (
+						<Location>
+							{({ location: { action, pathname } }) => (
+								<div className="tl-edges">
+									<TransitionGroup component={null}>
+										<DelayedTransition
+											key={pathname} // we're using seconds but transitiongroup uses ms
+											delay={getMs(entryDelay)}
+											timeout={{
+												enter: getMs(entryLength),
+												exit: getMs(exitLength),
+											}}
+											onEnter={node =>
+												!!node &&
+												!window.__tl_back_button_pressed &&
+												onEnter({
+													node,
+													action,
+													inTransition,
+													entryTrigger,
+													entryProps,
+													exitProps,
+													pathname,
+													updateContext,
+													triggerResolve,
+													appearAfter: getMs(
+														appearAfter,
+													),
+													e,
+												})
+											}
+											onExit={node =>
+												!!node &&
+												!window.__tl_back_button_pressed &&
+												onExit({
+													node,
+													inTransition,
+													exitTrigger,
+													entryProps,
+													exitProps,
+													triggerResolve,
+													e,
+												})
+											}
+										>
+											{transitionStatus => {
+												const mount =
+													transitionStatus ===
+														"entering" ||
+													transitionStatus ===
+														"entered"
 
-                          const states = {
-                            entry: {
-                              state: entryState,
-                              delay: entryDelay,
-                              length: entryLength
-                            },
-                            exit: {
-                              state: exitState,
-                              delay: exitDelay,
-                              length: exitLength
-                            }
-                          };
+												const states = {
+													entry: {
+														state: entryState,
+														delay: entryDelay,
+														length: entryLength,
+													},
+													exit: {
+														state: exitState,
+														delay: exitDelay,
+														length: exitLength,
+													},
+												}
 
-                          const current = mount ? states.entry : states.exit;
+												const current = mount
+													? states.entry
+													: states.exit
 
-                          const transitionState = returnTransitionState({
-                            inTransition,
-                            location: props.location,
-                            transitionIdHistory,
-                            transitionStatus,
-                            current,
-                            mount,
-                            ...states
-                          });
+												const transitionState = returnTransitionState(
+													{
+														inTransition,
+														location:
+															props.location,
+														transitionIdHistory,
+														transitionStatus,
+														current,
+														mount,
+														...states,
+													},
+												)
 
-                          const exitZindex = exitProps.zIndex || 0;
-                          const entryZindex = entryProps.zIndex || 1;
+												const exitZindex =
+													exitProps.zIndex || 0
+												const entryZindex =
+													entryProps.zIndex || 1
 
-                          return (
-                            <TransitionRenderer
-                              mount={mount}
-                              entryZindex={entryZindex}
-                              exitZindex={exitZindex}
-                              transitionStatus={transitionStatus}
-                              transitionState={transitionState}
-                              children={children}
-                              appearAfter={getMs(appearAfter)}
-                            />
-                          );
-                        }}
-                      </DelayedTransition>
-                    </TransitionGroup>
-                  </div>
-                </Layout>
-              )}
-            </Location>
-          );
-        }}
-      </Consumer>
-    );
-  }
+												return (
+													<TransitionRenderer
+														mount={mount}
+														entryZindex={
+															entryZindex
+														}
+														exitZindex={
+															exitZindex
+														}
+														transitionStatus={
+															transitionStatus
+														}
+														transitionState={
+															transitionState
+														}
+														children={children}
+														appearAfter={getMs(
+															appearAfter,
+														)}
+													/>
+												)
+											}}
+										</DelayedTransition>
+									</TransitionGroup>
+								</div>
+							)}
+						</Location>
+					)
+				}}
+			</Consumer>
+		)
+	}
 }
