0c5d84570b2b84d8894f4b3bb35b058559e41b2b
Tyler Barnes <tylerdbarnes@gmail.com>
2019-10-31 09:20:20 -0700

    fix: hotfix for safari where matchMedia.addEventListener is not a function

--- src/context/InternalProvider.js
+++ src/context/InternalProvider.js
@@ -1,55 +1,65 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Provider } from './createTransitionContext';
-import getPagesPromises from '../utils/getPagesPromises';
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { Provider } from "./createTransitionContext"
+import getPagesPromises from "../utils/getPagesPromises"
 
 class InternalProvider extends Component {
-  constructor(props) {
-    super(props);
-    const prefersReducedMotion =
-      typeof window !== `undefined` &&
-      window.matchMedia('(prefers-reduced-motion: reduce)');
+	constructor(props) {
+		super(props)
+		const prefersReducedMotion =
+			typeof window !== `undefined` &&
+			window.matchMedia("(prefers-reduced-motion: reduce)")
 
-    this.state = {
-      inTransition: false,
-      transitionIdHistory: [],
-      disableAnimation: prefersReducedMotion.matches,
-      // event
-      e: false,
-      // exit
-      exitDelay: 0,
-      exitLength: 0,
-      exitState: {},
-      exitProps: {},
-      exitTrigger: false,
-      // entry
-      entryDelay: 0,
-      entryLength: 0,
-      entryState: {},
-      entryProps: {},
-      entryTrigger: false,
-      // state updates
-      updateContext: obj => this.setState(obj)
-    };
+		this.state = {
+			inTransition: false,
+			transitionIdHistory: [],
+			disableAnimation: prefersReducedMotion.matches,
+			// event
+			e: false,
+			// exit
+			exitDelay: 0,
+			exitLength: 0,
+			exitState: {},
+			exitProps: {},
+			exitTrigger: false,
+			// entry
+			entryDelay: 0,
+			entryLength: 0,
+			entryState: {},
+			entryProps: {},
+			entryTrigger: false,
+			// state updates
+			updateContext: obj => this.setState(obj),
+		}
 
-    if (typeof window !== `undefined`) {
-      	prefersReducedMotion.addEventListener("change", () => {
-          this.setState({ disableAnimation: prefersReducedMotion.matches })
-        })
-    }
-  }
+		if (
+			prefersReducedMotion &&
+			typeof prefersReducedMotion.addEventListener === `function`
+		) {
+			prefersReducedMotion.addEventListener("change", () => {
+				this.setState({ disableAnimation: prefersReducedMotion.matches })
+			})
+		} else if (
+			prefersReducedMotion &&
+			typeof prefersReducedMotion.addListener === `function`
+		) {
+			prefersReducedMotion.addListener(() => {
+				this.setState({ disableAnimation: prefersReducedMotion.matches })
+			})
+		}
+	}
 
-  componentDidMount() {
-    this.state.updateContext(getPagesPromises());
-  }
+	componentDidMount() {
+		this.state.updateContext(getPagesPromises())
+	}
 
-  render() {
-    return <Provider value={this.state}>{this.props.children}</Provider>;
-  }
+	render() {
+		return <Provider value={this.state}>{this.props.children}</Provider>
+	}
 }
 
 InternalProvider.propTypes = {
-  children: PropTypes.node.isRequired
-};
+	children: PropTypes.node.isRequired,
+}
 
-export default InternalProvider;
+export default InternalProvider
