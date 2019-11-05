7fcf45bb50983b87ac12c5ceb843659f77ab2f76
Tyler Barnes <tylerdbarnes@gmail.com>
2019-11-05 14:59:10 -0800

    pass plugin options down to page renderer to disable injecting page props into pages

--- src/components/TransitionRenderer.js
+++ src/components/TransitionRenderer.js
@@ -1,63 +1,67 @@
-import React, { Component, cloneElement } from "react";
-import { setTimeout, clearTimeout } from "requestanimationframe-timer";
-import { PublicProvider } from "../context/createTransitionContext";
+import React, { Component, cloneElement } from "react"
+import { setTimeout, clearTimeout } from "requestanimationframe-timer"
+import { PublicProvider } from "../context/createTransitionContext"
 
 export default class TransitionRenderer extends Component {
-  state = {
-    shouldBeVisible: !!!this.props.appearAfter
-  };
+	state = {
+		shouldBeVisible: !!!this.props.appearAfter,
+	}
 
-  shouldComponentUpdate(nextProps, nextState) {
-    // only rerender if the transition status changes.
-    return (
-      this.props.transitionStatus !== nextProps.transitionStatus ||
-      this.state.shouldBeVisible !== nextState.shouldBeVisible
-    );
-  }
+	shouldComponentUpdate(nextProps, nextState) {
+		// only rerender if the transition status changes.
+		return (
+			this.props.transitionStatus !== nextProps.transitionStatus ||
+			this.state.shouldBeVisible !== nextState.shouldBeVisible
+		)
+	}
 
-  componentDidMount = () => {
-    const delay = typeof this.props.delay === "number" ? this.props.delay : 0;
-    const appearafter =
-      typeof this.props.appearAfter === "number" ? this.props.appearAfter : 0;
-    const timeout = delay + appearafter;
+	componentDidMount = () => {
+		const delay = typeof this.props.delay === "number" ? this.props.delay : 0
+		const appearafter =
+			typeof this.props.appearAfter === "number" ? this.props.appearAfter : 0
+		const timeout = delay + appearafter
 
-    this.appearTimeout = setTimeout(
-      () => this.setState({ shouldBeVisible: true }),
-      timeout
-    );
-  };
+		this.appearTimeout = setTimeout(
+			() => this.setState({ shouldBeVisible: true }),
+			timeout,
+		)
+	}
 
-  componentWillUnmount = () => {
-    clearTimeout(this.appearTimeout);
-  };
+	componentWillUnmount = () => {
+		clearTimeout(this.appearTimeout)
+	}
 
-  render() {
-    const {
-      mount,
-      entryZindex,
-      exitZindex,
-      transitionStatus,
-      transitionState,
-      children
-    } = this.props;
+	render() {
+		const {
+			mount,
+			entryZindex,
+			exitZindex,
+			transitionStatus,
+			transitionState,
+			children,
+			injectPageProps,
+		} = this.props
 
-    return (
-      <div
-        className={`tl-wrapper ${
-          mount ? "tl-wrapper--mount" : "tl-wrapper--unmount"
-        } tl-wrapper-status--${transitionStatus}`}
-        style={{
-          zIndex: mount ? entryZindex : exitZindex,
-          opacity: this.state.shouldBeVisible ? 1 : 0
-        }}
-      >
-        <PublicProvider value={{ ...transitionState }}>
-          {/* pass transition state to page/template */}
-          {cloneElement(children, {
-            ...transitionState
-          })}
-        </PublicProvider>
-      </div>
-    );
-  }
+		return (
+			<div
+				className={`tl-wrapper ${
+					mount ? "tl-wrapper--mount" : "tl-wrapper--unmount"
+				} tl-wrapper-status--${transitionStatus}`}
+				style={{
+					zIndex: mount ? entryZindex : exitZindex,
+					opacity: this.state.shouldBeVisible ? 1 : 0,
+				}}
+			>
+				<PublicProvider value={{ ...transitionState }}>
+					{/* pass transition state to page/template */}
+					{// injectPageProps is a plugin option
+					injectPageProps
+						? cloneElement(children, {
+								...transitionState,
+						  })
+						: children}
+				</PublicProvider>
+			</div>
+		)
+	}
 }
