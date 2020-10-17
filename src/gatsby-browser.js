48f8eac73c70a1c6b214ec21d26facb38b897c29
Tyler Barnes <tylerdbarnes@gmail.com>
2020-10-17 01:01:14 -0700

    fix plugin for latest Gatsby

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,17 +1,4 @@
-const { navigate } = require('gatsby')
-
 exports.wrapPageElement = require(`./wrap-page`)
 exports.wrapRootElement = require(`./wrap-root`)
 
 exports.shouldUpdateScroll = () => !window.__tl_inTransition
-
-exports.onPreRouteUpdate = ({ location }) => {
-	// prevent the back button during transitions as it breaks pages
-	if (
-		window.__tl_inTransition &&
-		location.pathname !== window.__tl_desiredPathname
-	) {
-		window.__tl_back_button_pressed = true
-		navigate(window.__tl_desiredPathname)
-	}
-}
